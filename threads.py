ce4152edfb07af0a913b6cc12c89ab41b00d24f6
Erik Krook <erikkrook@hotmail.se>
2025-07-20 00:19:54 +0200

    added multi threading to the experiment

--- threads.py
+++ threads.py
@@ -14,3 +14,23 @@ def singel_threaded():
     print(f"Singel-threaded total time: {end - start:.2f} seconds")
 
 singel_threaded()
+
+import threading
+
+
+def multi_threading():
+    start = time.time()
+
+    threads = []
+    for name in ["A", "B", "C"]:
+        t = threading.Thread(target=task, args=(name,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+    end = time.time()
+    print(f"Multi-threded total time: {end - start:.2f} seconds")
+
+multi_threading()
