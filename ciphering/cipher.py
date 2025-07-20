4591b95ed89b6413ed5f24f9a2b0130d5f74c737
Erik Krook <erikkrook@hotmail.se>
2025-07-20 14:50:34 +0200

    added vigenere

--- ciphering/cipher.py
+++ ciphering/cipher.py
@@ -15,3 +15,34 @@ def decrypt_ceasar_cipher(text, shift):
     
     return decrypted
 
+def encrypt_vigenere(text, key):
+    encrypted = ""
+    key_length = len(key)
+
+    for i in range(len(text)):
+        char = text[i]
+        key_char = key[i % key_length]  # loop over the key
+        encrypted_char = chr((ord(char) + ord(key_char)) % 256)  # Use %256 for full byte range
+        encrypted += encrypted_char
+
+    return encrypted
+
+def decrypt_vigenere(text, key):
+    decrypted = ""
+    key_length = len(key)
+
+    for i in range(len(text)):
+        char = text[i]
+        key_char = key[i % key_length]
+        decrypted_char = chr((ord(char) - ord(key_char)) % 256)
+        decrypted += decrypted_char
+
+    return decrypted
+
+key = 'lumpa'
+
+encoded = encrypt_vigenere('hej', key)
+
+print(encoded)
+
+print(decrypt_vigenere(encoded, key))
--- ciphering/test_cipher.py
+++ ciphering/test_cipher.py
@@ -1,23 +1,45 @@
-from cipher import encrypt_ceasar_cipher, decrypt_ceasar_cipher
+from cipher import encrypt_ceasar_cipher, decrypt_ceasar_cipher, encrypt_vigenere, decrypt_vigenere
 
-def test_basic_encrypt_decrypt():
+def test_ceasar_encrypt_decrypt():
     original = "hej"
     encrypted = encrypt_ceasar_cipher(original, 3)
     decrypted = decrypt_ceasar_cipher(encrypted, 3)
     assert decrypted == original
 
-def test_empty_string():
+def test_ceasar_empty_string():
     original = ""
     encrypted = encrypt_ceasar_cipher(original, 3)
     decrypted = decrypt_ceasar_cipher(encrypted, 3)
     assert decrypted == original
 
-def test_known_values():
+def test_ceasar_known_values():
     assert encrypt_ceasar_cipher("abc", 3) == "def"
     assert decrypt_ceasar_cipher("def", 3) == "abc"
 
-def test_wrong_encrypt():
+def test_ceasar_wrong_encrypt():
     original = "hej"
     encrypted = encrypt_ceasar_cipher(original, 3)
     decrypted = decrypt_ceasar_cipher(encrypted, 4)
+    assert encrypted != decrypted
+
+def test_vigenere_encrypt_decrypt():
+    original = "hej"
+    key = 'liverpool'
+    encrypted = encrypt_vigenere(original, key)
+    decrypted = decrypt_vigenere(encrypted, key)
+    assert decrypted == original
+
+def test_vigenere_empty_string():
+    original = ""
+    key = 'liverpool'
+    encrypted = encrypt_vigenere(original, key)
+    decrypted = decrypt_vigenere(encrypted, key)
+    assert decrypted == original
+
+
+def test_cvigenere_wrong_encrypt():
+    original = "hej"
+    key = 'liverpool'
+    encrypted = encrypt_vigenere(original, key)
+    decrypted = decrypt_vigenere(encrypted, 'aik')
     assert encrypted != decrypted
